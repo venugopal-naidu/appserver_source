e2c619d27094bedfc23436933f73634d7bea2ee3
roopesh <rsakala@care.com>
2016-03-22 00:36:55 +0530

    api changes, doctor get

--- grails-app/controllers/com/vellkare/core/SearchController.groovy
+++ grails-app/controllers/com/vellkare/core/SearchController.groovy
@@ -1,5 +1,6 @@
 package com.vellkare.core
 
+import com.vellkare.util.BeanUtil
 import org.codehaus.groovy.grails.validation.Validateable
 
 import javax.ws.rs.Produces
@@ -51,57 +52,7 @@ class SearchController {
     respond (location: cmd.location, specialty: cmd.specialty, hospitals: hospitals)
   }
 
-  def transformObject (def fromType , def toType, def fromObj){
 
-    if(fromObj instanceof Doctor && toType == DoctorHospitalMini) {
-      def doctor = fromObj
-      DoctorHospitalMini d = new DoctorHospitalMini()
-      ['id','name', 'description', 'website', 'velkareVerified',
-       'degree1', 'univ1', 'degree2', 'univ2', 'degree3', 'univ3',
-       'degree4', 'univ4', 'degree5', 'univ5', 'experience', 'awards', 'language'].each { name ->
-        def val = doctor."${name}"
-        d."${name}" = val
-      }
-
-      if (doctor.hospitals) {
-        d.hospitals = doctor.hospitals.collect { DoctorHospital dh ->
-          HospitalAvailabilityMini mini = new HospitalAvailabilityMini()
-          mini.id = dh.hospital.id
-          mini.name = dh.hospital.name
-          mini.address1 = dh.hospital.address1
-          mini.address2 = dh.hospital.address2
-          mini.address3 = dh.hospital.address3
-          mini.address4 = dh.hospital.address4
-          ["MONDAY", 'TUESDAY', 'WEDNESDAY', 'THURSDAY', 'FRIDAY','SATURDAY','SUNDAY'].each {
-            mini.availability.put ( it,[new AvailabilityMini(from: dh.serviceHoursFrom, to: dh.serviceHoursTo)] )
-          }
-          mini
-        }
-      }
-      if(doctor.specialities){
-        d.specialties = doctor.specialities.collect { it.speciality.name.toLowerCase().capitalize() }
-      }
-      def photoUrl = grailsApplication.config.images.doctors.path?:"/images/doctor/"
-      photoUrl= "${photoUrl}${doctor.id}.jpeg"
-      d.photoUrl=photoUrl
-      return d
-    }
-    if(fromObj instanceof Lab && toType == LabMini) {
-      def lab = fromObj
-      LabMini d = new LabMini()
-      ['id', 'name', 'address1', 'address2',
-       'address3', 'address4', 'state', 'city', 'country', 'postalCode',
-       'website','velkareVerified'].each { name ->
-        def val = lab."${name}"
-        d."${name}" = val
-      }
-      def photoUrl = grailsApplication.config.images.labs.path?:"/images/lab/"
-      photoUrl= "${photoUrl}${lab.id}.jpeg"
-      d.labImageUrl = photoUrl
-      return d
-    }
-    return null
-  }
 
   def listHospitalSpecialities(SearchCommand cmd){
     cmd.location = cmd.location?:'Hyderabad'
@@ -137,7 +88,7 @@ class SearchController {
         ilike('hospital.specialists', "%${cmd.specialty}%")
       }
     }.collect{ doctor->
-      transformObject(doctor.class, DoctorHospitalMini, doctor)
+      BeanUtil.transformObject(doctor.class, DoctorHospitalMini, doctor)
     }
 
     respond (location: cmd.location, hospital: cmd.hospital, specialty: cmd.specialty,
@@ -199,7 +150,7 @@ class SearchController {
       if(cmd.labName)
         ilike('l.name', "%${cmd.labName}%")
 
-    }.collect { transformObject(Lab, LabMini, it)}
+    }.collect { BeanUtil.transformObject(Lab, LabMini, it)}
     respond( testName :cmd.testName, labName:cmd.labName, location:cmd.location, labs: labs )
   }
 
